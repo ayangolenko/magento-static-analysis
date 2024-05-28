63fe75804fae38b94bf449acfa9c224b53698b7a
Artem Yanholenko <ayang@softserveinc.com>
2024-05-28 18:40:30 +0200

    Update test configuration and add README files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Download
+`curl -s https://raw.githubusercontent.com/ayangolenko/magento-static-analyzers/main/bin/download | bash`
--- bin/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-[ -z "$1" ] && echo "Please the download directory path" && exit
-
-curl -s https://raw.githubusercontent.com/ayangolenko/magento-static-analyzers/main/test.txt "$1" | bash
